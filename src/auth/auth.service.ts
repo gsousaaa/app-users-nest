992f6a1da2067a85b457dd2c58120843142190c1
glaucobluedev <glauco@sounuv.com>
2025-06-21 16:52:16 -0300

    feat: AuthService

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/db/entities/User';
+import { Repository } from 'typeorm';
+import { LoginDto } from './dto/login-dto';
+import { HashService } from 'src/common/adapters/bcrypt.adapter';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from './dto/create-user-dto';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(User) private userRepository: Repository<User>,
+        @Inject('Hasher') private readonly hasher: HashService,
+        private readonly jwtService: JwtService
+    ) { }
+
+    async login(data: LoginDto) {
+        const user = await this.userRepository.findOneBy({ email: data.email })
+
+        const matchPassword = this.hasher.compare(data.password, user!.password)
+
+        if (!user || !matchPassword) throw new BadRequestException(`Usuário e/ou senha incorretos!`)
+
+        const payload = {
+            id: user.id,
+            role: user.role,
+            email: user.email,
+            name: user.name
+        }
+
+        const accessToken = this.jwtService.signAsync(payload, { expiresIn: '1h', secret: process.env.JWT_SECRET_KEY })
+        return { accessToken }
+    }
+
+    async register(data: CreateUserDto) {
+        const existsUser = await this.userRepository.findOneBy({ email: data.email })
+
+        if (existsUser) throw new BadRequestException('Usuário já cadastrado')
+
+        const createdUser = await this.userRepository.save(data)
+
+        const payload = {
+            id: createdUser.id,
+            role: createdUser.role,
+            email: createdUser.email,
+            name: createdUser.name
+        }
+
+        const accessToken = this.jwtService.signAsync(payload, { expiresIn: '1h', secret: process.env.JWT_SECRET_KEY })
+
+        return { createdUser, accessToken }
+    }
+}
