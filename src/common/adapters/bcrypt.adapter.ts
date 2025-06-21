2227dd3cba52cf773c0ca7cbec75f46f4d8f5178
glaucobluedev <glauco@sounuv.com>
2025-06-21 16:52:37 -0300

    feat: Bcrypt Adapter

---
+++ src/common/adapters/bcrypt.adapter.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+export interface HashService {
+    hash(plain: string): Promise<string>,
+    compare(plain: string, hashed: string): Promise<Boolean>
+}
+
+
+@Injectable()
+export class BcryptAdapter implements HashService {
+    constructor(private readonly salt = 10) { }
+
+    async hash(plain: string): Promise<string> {
+        return bcrypt.hash(plain, this.salt)
+    }
+
+    async compare(plain: string, hashed: string) {
+        return bcrypt.compare(plain, hashed)
+    }
+}
