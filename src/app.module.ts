9567fccb128263e029b617ec95e76311b3e64297
glaucobluedev <glauco@sounuv.com>
2025-06-22 15:12:12 -0300

    chore: changing methods path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { AuthController } from './modules/auth/auth.controller';
+import { AuthService } from './modules/auth/auth.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersController } from './modules/users/users.controller';
+import { UsersService } from './modules/users/users.service';
 import { AuthMiddleware } from './common/middlewares/AuthMiddleware';
 
 @Module({
