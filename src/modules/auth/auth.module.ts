48c87668b2534721ce3ac3cd72cfa3d5c9eb0915
glaucobluedev <glauco@sounuv.com>
2025-06-22 18:52:30 -0300

    feat: import typeorm module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { BcryptAdapter } from 'src/common/adapters/bcrypt.adapter';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/db/entities/User';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([User]), JwtModule.register({
+        secret: process.env.JWT_SECRET_KEY,
+        signOptions: { expiresIn: '1h' },
+    }), ConfigModule],
     controllers: [AuthController],
     providers: [AuthService, { provide: 'Hasher', useClass: BcryptAdapter }]
 })
