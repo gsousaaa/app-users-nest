ed737a95583ed8853c5b22a60b137b3e252995b8
glaucobluedev <glauco@sounuv.com>
2025-06-23 02:17:54 -0300

    fix: inset users query

--- src/db/queries/insert-users.sql
+++ src/db/queries/insert-users.sql
@@ -1,5 +1,5 @@
-INSERT INTO "user" (name, email, password, role, last_login))
+INSERT INTO "user" (name, email, password, role, last_login)
 VALUES 
   ('Admin teste', 'admin@example.com', '$2a$10$8US6JcMlhvMpr4FT9nYpP.gAQKdqngUeUuYpUSLRkeTzFgfZ7bddi', 'admin', NOW()),
-  ('User teste', 'user@example.com', '$2a$10$8US6JcMlhvMpr4FT9nYpP.gAQKdqngUeUuYpUSLRkeTzFgfZ7bddi', 'user', NOW());
-    ('Inactive user', 'inactiveuser@example.com', '$2a$10$8US6JcMlhvMpr4FT9nYpP.gAQKdqngUeUuYpUSLRkeTzFgfZ7bddi', 'user', NOW() - INTERVAL '1 month');
+  ('User teste', 'user@example.com', '$2a$10$8US6JcMlhvMpr4FT9nYpP.gAQKdqngUeUuYpUSLRkeTzFgfZ7bddi', 'user', NOW()),
+  ('Inactive user', 'inactiveuser@example.com', '$2a$10$8US6JcMlhvMpr4FT9nYpP.gAQKdqngUeUuYpUSLRkeTzFgfZ7bddi', 'user', NOW() - INTERVAL '1 month');
