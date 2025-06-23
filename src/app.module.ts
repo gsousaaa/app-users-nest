2f2acba1ef15c6805a6cb7e1e1a2803ab06f0176
glaucobluedev <glauco@sounuv.com>
2025-06-23 00:45:15 -0300

    feat: jwt service config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,19 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './modules/auth/auth.controller';
-import { AuthService } from './modules/auth/auth.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersController } from './modules/users/users.controller';
-import { UsersService } from './modules/users/users.service';
 import { AuthMiddleware } from './common/middlewares/AuthMiddleware';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { parseEnv } from './env';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './db/config';
 import { UsersModule } from './modules/users/users.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    JwtModule,
     AuthModule,
     UsersModule,
     ConfigModule.forRoot({ isGlobal: true, validate: parseEnv }),
@@ -24,7 +23,12 @@ import { UsersModule } from './modules/users/users.module';
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  constructor(private readonly jwtService: JwtService) { }
+
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes(UsersController)
+    consumer.apply((req, res, next) => {
+      const middleware = new AuthMiddleware(this.jwtService)
+      middleware.use(req, res, next)
+    }).forRoutes(UsersController)
   }
 }
