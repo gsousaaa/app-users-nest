e2cdc08204c0a488a7b86d1c7038d0ecd4894976
glaucobluedev <glauco@sounuv.com>
2025-06-22 23:59:36 -0300

    feat: insert users query

---
+++ src/db/queries/insert-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO "user" (name, email, password, role)
+VALUES 
+  ('Admin teste', 'admin@example.com', '$2a$10$8US6JcMlhvMpr4FT9nYpP.gAQKdqngUeUuYpUSLRkeTzFgfZ7bddi', 'admin'),
+  ('User teste', 'user@example.com', '$2a$10$8US6JcMlhvMpr4FT9nYpP.gAQKdqngUeUuYpUSLRkeTzFgfZ7bddi', 'user');
